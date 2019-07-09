346a55d7adff20a0782b98fd1420909486941ffc
HarryProject <nuintuition@gmail.com>
2019-07-10 01:05:58 +0800

    1.建立CURD API 2.建立DB model 3.完成Read功能

---
+++ CURDDemo/CURDDemo/Controllers/CURDController.cs
@@ -0,0 +1,74 @@
+﻿using CURDDemo.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CURDDemo.Controllers {
+    public class CURDController : Controller {
+        private IDbConnection _db = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ConnectionString);
+        
+        // GET: CURD
+        [HttpGet]
+        public ActionResult Index() {
+            List<MyTableModel> myTableList = _db.Query<MyTableModel>("SELECT * FROM MyTable").ToList();
+            return View(myTableList);
+        }
+
+        // GET: CURD/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: CURD/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection) {
+            try {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: CURD/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: CURD/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection) {
+            try {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+
+        // GET: CURD/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: CURD/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection) {
+            try {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            } catch {
+                return View();
+            }
+        }
+    }
+}
---
+++ CURDDemo/CURDDemo/Models/MyTableModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CURDDemo.Models {
+    public class MyTableModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
