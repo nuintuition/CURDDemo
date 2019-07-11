5bdb97235bef11c0c8fc88237148f069251a1a2c
HarryProject <nuintuition@gmail.com>
2019-07-11 21:24:37 +0800

    調整初始頁面

--- CURDDemo/CURDDemo/App_Start/RouteConfig.cs
+++ CURDDemo/CURDDemo/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace CURDDemo {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CURD", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
